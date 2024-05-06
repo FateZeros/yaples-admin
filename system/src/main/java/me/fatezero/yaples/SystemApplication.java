92960e8b2ec28ef8cfabb06382ecaad6e43050a4
yejianfei <yejianfei@egova.com.cn>
2024-05-06 20:04:20 +0800

    init

---
+++ system/src/main/java/me/fatezero/yaples/SystemApplication.java
@@ -0,0 +1,13 @@
+package me.fatezero.yaples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemApplication.class, args);
+    }
+
+}
---
+++ system/src/test/java/me/fatezero/yaples/SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package me.fatezero.yaples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
