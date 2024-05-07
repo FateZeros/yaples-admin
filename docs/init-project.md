47f1063c92e38910feba0191aa3e86ecf3b5784a
FateZeros <490293266@qq.com>
2024-05-07 09:49:27 +0800

    update

--- docs/init-project.md
+++ docs/init-project.md
@@ -2,6 +2,12 @@
 
 ## 分模块搭建 spring 项目
 
-1. 选中父 pom.xml，使用 `new module` 创建 spring 项目
+选中父 pom.xml，使用 `new module` 创建 spring 项目
 
 <strong>注意 `groupId` 和 `packageName` 需要保持一致。</strong>
+
+## GAV 三元组
+
+Group: 是公司或者组织或者个人的名称，是一种命名空间的概念，比如网站，那么 group 可以是 tech/org/com/me 开头。
+Artifact: 当前项目的唯一标识。
+Version: 项目的版本号，一般xx-SNAPSHOT表示非稳定版
--- system/src/main/java/me/fatezero/yaples/SystemApplication.java
+++ system/src/main/java/me/fatezero/yaples/SystemApplication.java
@@ -2,12 +2,24 @@ package me.fatezero.yaples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SystemApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(SystemApplication.class);
+        // 监控应用的 PID，启动时可指定 PID 路径：--spring.pid.file=/home/yaples-admin/app.pid
+        // 或者在 application.yml 添加文件路径，方便 kill，kill `cat /home/yaples-admin/app.pid`
+        springApplication.addListeners(new ApplicationPidFileWriter());
+        springApplication.run(args);
     }
 
+    @GetMapping("/")
+    public String index() {
+        return "Backend service start success";
+    }
 }
