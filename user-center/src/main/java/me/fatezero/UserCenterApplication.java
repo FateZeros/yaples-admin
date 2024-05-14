b4dab1b058a9ac30f79e57593fc0aac99a3e5723
FateZeros <490293266@qq.com>
2024-05-14 09:45:09 +0800

    update

--- user-center/src/main/java/me/fatezero/UserCenterApplication.java
+++ user-center/src/main/java/me/fatezero/UserCenterApplication.java
@@ -2,7 +2,9 @@ package me.fatezero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class UserCenterApplication {
     public static void main(String[] args) {
--- user-center/src/main/java/me/fatezero/controller/UserController.java
+++ user-center/src/main/java/me/fatezero/controller/UserController.java
@@ -5,7 +5,6 @@ import me.fatezero.entity.query.UserQueryBean;
 import me.fatezero.service.UserService;
 import me.fatezero.utils.response.ResponseResult;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -14,13 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
     @Autowired
     private UserService userService;
 
+    /**
+     * @return user list
+     */
+    @ApiOperation("Query User Page")
     @GetMapping("list")
     public ResponseResult<Page<User>> list(@RequestParam int pageSize, @RequestParam int pageNumber) {
         return ResponseResult.success(userService.findPage(UserQueryBean.builder().build(), PageRequest.of(pageNumber, pageSize)));
